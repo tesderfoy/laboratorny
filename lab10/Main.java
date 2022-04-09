b46005cd1df67009fb3ca6313f5fbe6c388dee05
tesderfoy <98217566+tesderfoy@users.noreply.github.com>
2022-04-09 10:05:44 +0300

    Add files via upload

---
+++ lab10/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+class ShortWords{
+    public ShortWords(String a) {
+        String result;
+        String[] words = a.split(" ");
+        int max = 100000;
+        for (String word : words) {
+            int min = word.length();
+            if (min< max){
+                max = min;
+            }
+        }
+        for (String array : words){
+            if(array.length() == max){
+                result = array;
+                System.out.println(array);
+                break;
+            }
+        }
+    }
+}
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+    String a = in.nextLine();
+    ShortWords ds = new ShortWords(a);
+
+    }
+}
